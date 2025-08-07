f0360c0c63646fb6f90fdbf37d29661c49e6a1bc
Alvaro0098 <101212585+Alvaro0098@users.noreply.github.com>
2025-08-07 18:45:19 -0300

    design system basic

---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    primary: "#0DB4A1",
+    secondary: "#502EAC",
+    gray: "#808080",
+    dark: "#282828",
+    darkGray: "#676262",
+  },
+  typography: {
+    normalText: {
+      fontFamily: '"M PLUS 2", sans-serif',
+      color: "#808080",
+      letterSpacing: "0.16rem",
+      fontWeight: 400,
+    },
+    subtitle: {
+      fontFamily: '"M PLUS 2", sans-serif',
+      color: "#676262",
+      letterSpacing: "0.16rem",
+      fontWeight: 700,
+    },
+    title: {
+      fontFamily: '"lhivic", sans-serif',
+      color: "#282828",
+      letterSpacing: "0.96rem",
+      fontWeight: 200,
+    },
+  },
+};
